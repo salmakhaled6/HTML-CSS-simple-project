e957f2a5f053c8ad91f2f383507d4cc3142550b4
salmakhaled6 <121482046+salmakhaled6@users.noreply.github.com>
2024-06-29 09:11:11 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+//task 1
+
+// var sum = 0;
+// var userInput;
+
+// do {
+//     var value = prompt("Please enter a value");
+
+//     // Convert the entered value to a number
+//     userInput = parseFloat(value);
+
+//     // Check if userInput is a valid number
+//     if (!isNaN(userInput)) {
+//         sum += userInput;
+
+//         // Check if sum exceeds 100
+//         if (sum > 100) {
+//             alert("Sum exceeds 100");
+//             break; // Exit the loop if sum exceeds 100
+//         }
+//     } else {
+//         alert("Please enter a number");
+//     }
+
+// } while (userInput !== 0);
+
+// alert("Sum equals: " + sum);
+
+
+// task 2
+
+// alert("Welcome to my site ");
+
+// do{
+//  var name = prompt("enter your name ");
+// var color = prompt("red , green , blue ?" )
+
+// }while(isNaN(name))
+
+// alert("welcome"+name)
+
+
+
+
